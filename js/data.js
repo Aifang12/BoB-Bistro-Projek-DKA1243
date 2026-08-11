50bb4b71540d2ca11473a6256aae484894310ac6
aiman <muhdaimanasyraaf08@gmail.com>
2026-08-12 01:46:58 +0800

    Set & Drinks dummy data added

--- js/data.js
+++ js/data.js
@@ -4,44 +4,108 @@
 
 const foods=[
 
+//Set
 {
 id:1,
 name:"Nasi Ayam",
 price:12,
 rating:4.9,
 category:"Set",
-image:"images/foods/makanan.png",
+image:"images/foods/set/Nasi Ayam.png",
 description:"Ayam goreng rangup dihidangkan bersama nasi putih, sambal dan timun segar."
 },
 
 {
 id:2,
-name:"Burger Chicken",
-price:10,
-rating:4.8,
+name:"Nasi Lemak Ayam Goreng Berempah",
+price:22,
+rating:4.7,
 category:"Set",
-image:"images/foods/makanan.png",
-description:"Burger ayam bersama sayuran segar dan sos istimewa."
+image:"images/foods/set/Nasi Lemak Ayam Goreng Berempah.png",
+description:"Nasi lemak beras basmati beraroma santan dan daun pandan, dihidangkan bersama sambal tumis pedas manis, ayam goreng berempah, telur rebus, kacang, dan ikan bilis."
 },
 
 {
 id:3,
-name:"Spaghetti",
-price:16,
-rating:4.9,
-category:"Western",
-image:"images/foods/makanan.png",
-description:"Spaghetti bersama sos bolognese dan keju parmesan."
+name:"Nasi Daging Harimau Menangis",
+price:28,
+rating:5.0,
+category:"Set",
+image:"images/foods/set/Nasi Daging Harimau Menangis.png",
+description:"Nasi putih lembut dihidangkan bersama daging lembu bakar empuk, air asam utara yang padu, ulam-ulaman segar, dan sup kosong."
 },
 
+{
+id:4,
+name:"Chicken Chop Crispy Sos Lada Hitam",
+price:24,
+rating:4.8,
+category:"Set",
+image:"images/foods/set/Chicken Chop Crispy Sos Lada Hitam.png",
+description:"Kepingan paha ayam digoreng rangup, disiram sos lada hitam pekat, dihidangkan bersama kentang goreng dan coleslaw."
+},
+
+
+//Drinks
+{
+id:5,
+name:"Iced Sparkling Berry Lemonade",
+price:16,
+rating:4.9,
+category:"Minuman",
+image:"images/foods/drinks/Iced Sparkling Berry Lemonade.png",
+description:"Minuman soda berkarbonat segar digabungkan dengan pati beri dan perahan jus lemon segar."
+},
+
+{
+id:6,
+name:"Matcha Espresso Latte ",
+price:18,
+rating:4.6,
+category:"Minuman",
+image:"images/foods/drinks/Matcha Espresso Latte.png",
+description:"Gabungan matcha Jepun premium, susu segar, dan satu shot kopi espresso."
+},
+
+{
+id:7,
+name:"Classic Iced Peach Tea",
+price:14,
+rating:4.9,
+category:"Minuman",
+image:"images/foods/drinks/Classic Iced Peach Tea.png",
+description:"Teh ais segar dengan rasa buah pic manis dan hirisan buah segar."
+},
+
+{
+id:8,
+name:"B@B Signature Chocolate",
+price:16,
+rating:4.9,
+category:"Minuman",
+image:"images/foods/drinks/B@B Signature Chocolate.png",
+description:"Minuman coklat pekat panas atau ais yang dibuat daripada coklat artisanal premium."
+},
+
+//Dessert
 {
 id:4,
 name:"Mee Goreng",
 price:11,
 rating:4.7,
-category:"Mee",
+category:"Dessert",
+image:"images/foods/makanan.png",
+description:"Mee goreng dengan ayam dan sayuran segar."
+},
+
+//Snek
+{
+id:4,
+name:"Mee Goreng",
+price:11,
+rating:4.7,
+category:"Snek",
 image:"images/foods/makanan.png",
 description:"Mee goreng dengan ayam dan sayuran segar."
 }
-
 ];
